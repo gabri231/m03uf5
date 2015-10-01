9cc603a968eff5c83f22a54c1fb1579e93c88f91
Gabriel Calle Torrez <gabriel.calle92@gmail.com>
2015-10-01 18:51:14 +0200

    Project

---
+++ NF8-Llistes/project/Ingredient.java
@@ -0,0 +1,13 @@
+package project;
+public class Ingredient {
+	private int code;
+	private String name;
+	private double meassuring; 
+	private double kCal;
+	private double carbohidrates;
+	private double proteines;
+	private double fat;
+	private double salt;
+	private Allergen allergen;
+	
+}
