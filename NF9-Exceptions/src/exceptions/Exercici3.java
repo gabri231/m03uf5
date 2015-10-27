f8848dcd03254154a6899776ff137c0c0a127433
Gabriel Calle Torrez <gabriel.calle92@gmail.com>
2015-10-27 19:24:59 +0100

    commit

--- NF9-Exceptions/src/exceptions/Exercici3.java
+++ NF9-Exceptions/src/exceptions/Exercici3.java
@@ -16,7 +16,7 @@ public class Exercici3 {
 		}
 	}
 
-	static int divisio(int dividendo, int divisor){
+	static int divisio(int dividendo, int divisor) throws ArithmeticException{
 		if (divisor == 0){
 			throw new ArithmeticException();
 		}else{
