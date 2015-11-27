c1d08ac738d6cb5e7f649a70d63a3d9f2d626c2f
Gabriel Calle Torrez Indra <gabriel.calle92@gmail.com>
2015-11-27 01:43:09 +0100

    Metodos actualizados.

--- NF8-Mapes/src/nf8Conjunts/Exercici89.java
+++ NF8-Mapes/src/nf8Conjunts/Exercici89.java
@@ -21,122 +21,125 @@ import java.util.TreeMap;
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 public class Exercici89 {
-	///////////////////////////////////////////////////////////////////////////////////////////
-  	/////////////////////////////////////////////////////////////////////////////////////////////
-	///////  un mètode que permeti eliminar cotxes de més de 2500 cc de cilindrada  /////////////
-	// Creamos una clase estatica que elimina los pares de un mapa. Se eliminan los pares con cilindrada mayor a la que se passa por parametro
-	// Parametros 
-	//	--> int cilindrada = Cilindrada a partir de la cual borrar.
-	//  --> Map<String, Coche> mapCoches =  El mapa que contiene los pares a borrar.
-  	public static boolean eliminarCoche(int cilindrada, Map<String, Coche> mapCoches) {
-  		// Creamos una array list para guardar los key de los pares a borrar.
-		ArrayList<String> arrayMatriculas = new ArrayList<String>();
-		// Se recorre el mapa en busca de los pares que cumplan la condición a borrar.
-		for (Map.Entry<String, Coche> coche : mapCoches.entrySet()) {
-			// La condición es que la cilindrada de los pares sea mayor a la que se ha pasado por parametro.
-			if (coche.getValue().getCilindrada() >= cilindrada) {
-				// Cuando encuentre alguna coincidencia, el key se añade a el array de matriculas.
-				arrayMatriculas.add(coche.getKey());
-				// Imprimimos los datos de los pares a borrar.
-				System.out.print("Ha sido eliminado-►");
-				imprimirValueMap(coche);
+	static class Metodos{
+		///////////////////////////////////////////////////////////////////////////////////////////
+	  	/////////////////////////////////////////////////////////////////////////////////////////////
+		///////  un mètode que permeti eliminar cotxes de més de 2500 cc de cilindrada  /////////////
+		// Creamos una clase estatica que elimina los pares de un mapa. Se eliminan los pares con cilindrada mayor a la que se passa por parametro
+		// Parametros 
+		//	--> int cilindrada = Cilindrada a partir de la cual borrar.
+		//  --> Map<String, Coche> mapCoches =  El mapa que contiene los pares a borrar.
+	  	public static boolean eliminarCoche(int cilindrada, Map<String, Coche> mapCoches) {
+	  		// Creamos una array list para guardar los key de los pares a borrar.
+			ArrayList<String> arrayMatriculas = new ArrayList<String>();
+			// Se recorre el mapa en busca de los pares que cumplan la condición a borrar.
+			for (Map.Entry<String, Coche> coche : mapCoches.entrySet()) {
+				// La condición es que la cilindrada de los pares sea mayor a la que se ha pasado por parametro.
+				if (coche.getValue().getCilindrada() >= cilindrada) {
+					// Cuando encuentre alguna coincidencia, el key se añade a el array de matriculas.
+					arrayMatriculas.add(coche.getKey());
+					// Imprimimos los datos de los pares a borrar.
+					System.out.print("Ha sido eliminado-►");
+					imprimirValueMap(coche);
+				}
+			}
+			// REcorremos el mapa en caso de que contenga pares.
+			if(!mapCoches.isEmpty()){
+				// Borramos los pares a partir de la key guardada en nuestra array list.
+				for (String cocheMatricula : arrayMatriculas) {
+					mapCoches.remove(cocheMatricula);
+				}
+				// Devolvemos exito si ha borrado algo
+				return true;
+			}else{
+				// Devolvemos fracaso en caso de no haber borrado nada.
+				return false;
 			}
 		}
-		// REcorremos el mapa en caso de que contenga pares.
-		if(!mapCoches.isEmpty()){
-			// Borramos los pares a partir de la key guardada en nuestra array list.
-			for (String cocheMatricula : arrayMatriculas) {
-				mapCoches.remove(cocheMatricula);
-			}
-			// Devolvemos exito si ha borrado algo
-			return true;
-		}else{
-			// Devolvemos fracaso en caso de no haber borrado nada.
-			return false;
-		}
+
+	  	//////////////////////////////////////////////////////////////////////////////////////////////
+	  	/////////////////////////////////////////////////////////////////////////////////////////////
+		//  un mètode que com a paràmetre rebi un enter “nombre de cilindres” i retorni un ArrayList 
+	  	//  amb els cotxes que tinguin el nombre de cilindres passats.
+	  	//
+	  	//	Parametros:
+		//	--> int cilindros = El número de cilindros que tenga el value de los pares del mapa.
+		//  --> Map<String, Coche> mapCoches =  El mapa que contiene los pares a extraer en array.
+	  	public static ArrayList<Coche> mapDeCochesPorCilindro(int numCilindros, Map<String, Coche> mapDeCoches){
+	  		// Creamos el array para guardar los coches en un array list que será el que devolveremos con el metodo.
+	  		ArrayList<Coche> arrayRetorno = new ArrayList<Coche>();
+	  		// Comprobamos que no este vacio para recorrer el mapa.
+	  		if (!mapDeCoches.isEmpty()){
+	  			// Recorremos el mapa en busca de coches con numero de cilindros igual a los pasados por parametro
+		  		for (Map.Entry<String, Coche> coche : mapDeCoches.entrySet()){
+		  			// Buscamos coincidencias en número de cilindros
+		  			if (numCilindros == coche.getValue().getCilindres()){
+		  				// Si encuentra un coche con el mismo número de cilindros lo añade al array: arrayRetorno.
+		  				arrayRetorno.add(coche.getValue());
+		  			}
+		  		}
+		  		// Comprobamos que el array haya encontrado alguna coincidencia. Si está vacio devolvemos null.
+		  		if (arrayRetorno.isEmpty()){
+			  		// Una vez que se terminamos de recorrer el map de coches, devolvemos los resultados en formato arrayList.	
+		  			return null;	
+		  		}else{
+		  			return arrayRetorno;
+		  		}
+	  		}else{
+	  			// Si es que el mapa está vacio devuelve null.
+	  			return null;
+	  		}
+	  	}
+	  	
+		//////////////////////////////////////////////////////////////////////////////////////////////
+		/////////////////////////////////////////////////////////////////////////////////////////////
+	  	// 3. un mètode que retorni els noms dels cotxes agrupats per marques (amb un ArrayList de cotxes ja fem).
+
+	  	// Parametros
+		//  --> Map<String, Coche> mapCoches =  El mapa que contiene los pares a extraer en array.
+	  	//
+	  	// Retorno
+	  	// Map<String, ArrayList<Coche>>: String con las marcas de los coches. ArrayList de Coche 
+	  	public static Map<String, ArrayList<Coche>> mapCochesPorMarca(Map<String, Coche> mapDeCoches){
+	  		// SI el mapa que introducimos está vacio, devuelve null.
+	  		if (!mapDeCoches.isEmpty()){
+	  			// Se crea el ArrayList<String> temporal para introducir las marcas.
+		  		ArrayList<String> arrayMarca = new ArrayList<String>();
+		  		// Se recorre el mapa para introducir en nuestro ArrayList<String> las marcas de los coches
+		  		for (Map.Entry<String, Coche> coche : mapDeCoches.entrySet() ){
+		  			Coche marca = coche.getValue();
+		  			// Si el ArrayList<String> ya contiene una marca no se añade.
+		  			if (!arrayMarca.contains(marca.getMarca())){
+		  				arrayMarca.add(marca.getMarca());
+		  			}
+		  		}
+		  		// Se crea el mapa a devolver. Map<String, ArrayList<Coche>>
+		  		Map<String, ArrayList<Coche>> mapDeCochesOrdPorMarca = new TreeMap<String, ArrayList<Coche>>();
+		  		// Se recorre el array de marcas para introducirlo como Key en nuestro mapa.
+		  		for (int i=0; i<arrayMarca.size() ; i++){
+		  			String marca = arrayMarca.get(i);
+		  			// Se crea el ArrayList que será el Value de nuestro mapa.
+		  			ArrayList<Coche> arrayCoche = new ArrayList<Coche>();
+		  			// Recorremos el mapa con los coches, para buscar e introducir los coches de una marca.
+					for (Map.Entry<String, Coche> coche : mapDeCoches.entrySet() ){
+						// Se filtra los coches de una marca.
+						if (coche.getValue().getMarca() == marca){
+							// Los coches de una marca encontrada se añaden en el array de nuestro mapa.
+							arrayCoche.add(coche.getValue());
+						}
+					}
+					// Una vez con todos los coches de una marca, se añade la marca y los coches en el Mapa a devolver.
+					mapDeCochesOrdPorMarca.put(marca, arrayCoche);
+		  		}
+		  		// Una vez completo el Mapa se devuelve.
+		  		// Map<String, ArrayList<Coche>>
+		  		return mapDeCochesOrdPorMarca;
+	  		}else{
+	  			return null;
+	  		}
+	  	}
 	}
 
-  	//////////////////////////////////////////////////////////////////////////////////////////////
-  	/////////////////////////////////////////////////////////////////////////////////////////////
-	//  un mètode que com a paràmetre rebi un enter “nombre de cilindres” i retorni un ArrayList 
-  	//  amb els cotxes que tinguin el nombre de cilindres passats.
-  	//
-  	//	Parametros:
-	//	--> int cilindros = El número de cilindros que tenga el value de los pares del mapa.
-	//  --> Map<String, Coche> mapCoches =  El mapa que contiene los pares a extraer en array.
-  	public static ArrayList<Coche> mapDeCochesPorCilindro(int numCilindros, Map<String, Coche> mapDeCoches){
-  		// Creamos el array para guardar los coches en un array list que será el que devolveremos con el metodo.
-  		ArrayList<Coche> arrayRetorno = new ArrayList<Coche>();
-  		// Comprobamos que no este vacio para recorrer el mapa.
-  		if (!mapDeCoches.isEmpty()){
-  			// Recorremos el mapa en busca de coches con numero de cilindros igual a los pasados por parametro
-	  		for (Map.Entry<String, Coche> coche : mapDeCoches.entrySet()){
-	  			// Buscamos coincidencias en número de cilindros
-	  			if (numCilindros == coche.getValue().getCilindres()){
-	  				// Si encuentra un coche con el mismo número de cilindros lo añade al array: arrayRetorno.
-	  				arrayRetorno.add(coche.getValue());
-	  			}
-	  		}
-	  		// Comprobamos que el array haya encontrado alguna coincidencia. Si está vacio devolvemos null.
-	  		if (arrayRetorno.isEmpty()){
-		  		// Una vez que se terminamos de recorrer el map de coches, devolvemos los resultados en formato arrayList.	
-	  			return null;	
-	  		}else{
-	  			return arrayRetorno;
-	  		}
-  		}else{
-  			// Si es que el mapa está vacio devuelve null.
-  			return null;
-  		}
-  	}
-  	
-	//////////////////////////////////////////////////////////////////////////////////////////////
-	/////////////////////////////////////////////////////////////////////////////////////////////
-  	// 3. un mètode que retorni els noms dels cotxes agrupats per marques (amb un ArrayList de cotxes ja fem).
-
-  	// Parametros
-	//  --> Map<String, Coche> mapCoches =  El mapa que contiene los pares a extraer en array.
-  	//
-  	// Retorno
-  	// Map<String, ArrayList<Coche>>: String con las marcas de los coches. ArrayList de Coche 
-  	public static Map<String, ArrayList<Coche>> mapCochesPorMarca(Map<String, Coche> mapDeCoches){
-  		// SI el mapa que introducimos está vacio, devuelve null.
-  		if (!mapDeCoches.isEmpty()){
-  			// Se crea el ArrayList<String> temporal para introducir las marcas.
-	  		ArrayList<String> arrayMarca = new ArrayList<String>();
-	  		// Se recorre el mapa para introducir en nuestro ArrayList<String> las marcas de los coches
-	  		for (Map.Entry<String, Coche> coche : mapDeCoches.entrySet() ){
-	  			Coche marca = coche.getValue();
-	  			// Si el ArrayList<String> ya contiene una marca no se añade.
-	  			if (!arrayMarca.contains(marca.getMarca())){
-	  				arrayMarca.add(marca.getMarca());
-	  			}
-	  		}
-	  		// Se crea el mapa a devolver. Map<String, ArrayList<Coche>>
-	  		Map<String, ArrayList<Coche>> mapDeCochesOrdPorMarca = new TreeMap<String, ArrayList<Coche>>();
-	  		// Se recorre el array de marcas para introducirlo como Key en nuestro mapa.
-	  		for (int i=0; i<arrayMarca.size() ; i++){
-	  			String marca = arrayMarca.get(i);
-	  			// Se crea el ArrayList que será el Value de nuestro mapa.
-	  			ArrayList<Coche> arrayCoche = new ArrayList<Coche>();
-	  			// Recorremos el mapa con los coches, para buscar e introducir los coches de una marca.
-				for (Map.Entry<String, Coche> coche : mapDeCoches.entrySet() ){
-					// Se filtra los coches de una marca.
-					if (coche.getValue().getMarca() == marca){
-						// Los coches de una marca encontrada se añaden en el array de nuestro mapa.
-						arrayCoche.add(coche.getValue());
-					}
-				}
-				// Una vez con todos los coches de una marca, se añade la marca y los coches en el Mapa a devolver.
-				mapDeCochesOrdPorMarca.put(marca, arrayCoche);
-	  		}
-	  		// Una vez completo el Mapa se devuelve.
-	  		// Map<String, ArrayList<Coche>>
-	  		return mapDeCochesOrdPorMarca;
-  		}else{
-  			return null;
-  		}
-  	}
   	
 	public static void main(String[] args) {
 	  	//////////////////////////////////////////////////////////////////////////////////////////////
@@ -165,7 +168,7 @@ public class Exercici89 {
 		// Se llama al metodo que elimina los coches.
 		// Por parametro se pasa cilindrada y el mapa que contiene los pares a elegir por cilindrada y borrar.
 		System.out.println("Se eliminan los coches con cilindrada mayor a: " + cilindrada +"cc");
-		eliminarCoche(cilindrada, mapaDeCoches);
+		Metodos.eliminarCoche(cilindrada, mapaDeCoches);
 		
 		// Se comprueba qué coches se han borrado.
 		
@@ -181,7 +184,7 @@ public class Exercici89 {
 		ArrayList<Coche> arrayDeCochesPorCilindro = new ArrayList<Coche>();
 		// Guardamos en nuestro arrayList el resultado de los cilindros 
 		int cilindros = 4;
-		arrayDeCochesPorCilindro = mapDeCochesPorCilindro(cilindros,mapaDeCoches);
+		arrayDeCochesPorCilindro = Metodos.mapDeCochesPorCilindro(cilindros,mapaDeCoches);
 		System.out.println("Se imprime el array con los coches con "+ cilindros +" cilindros.");
 		imprimirArray(arrayDeCochesPorCilindro);
 		
@@ -190,7 +193,7 @@ public class Exercici89 {
 		// 3. un mètode que retorni els noms dels cotxes agrupats per marques (amb un ArrayList de cotxes ja fem). 
 		separador();
 		Map<String, ArrayList<Coche>> mapDeCochesOrdMarca = new TreeMap<String, ArrayList<Coche>>();
-		mapDeCochesOrdMarca = mapCochesPorMarca(mapaDeCoches);
+		mapDeCochesOrdMarca = Metodos.mapCochesPorMarca(mapaDeCoches);
 		if (!mapDeCochesOrdMarca.isEmpty()){
 			System.out.println("Se imprime el map con los coches agrupados por marca.");
 			imprimirMapStrArray(mapDeCochesOrdMarca);
